f7b6479e19f7f4e26b01abae637cd993577888c5
renuka.kharvi <renukak084@gmail.com>
2026-08-09 17:41:41 +0530

    update with better assersiom

--- tests/api/test_bitcoin_api.py
+++ tests/api/test_bitcoin_api.py
@@ -31,25 +31,50 @@ class TestBitcoinAPI:
         current_price=response_body["market_data"]["current_price"]
         market_cap=response_body["market_data"]["market_cap"]
         total_volume=response_body["market_data"]["total_volume"]
+        price_change_percentage_24h=response_body["market_data"]["price_change_percentage_24h"]
         homepage=response_body["links"]["homepage"]
         logger.info(f'{current_price}')
         logger.info(f'{market_cap}')
         logger.info(f'{total_volume}')
         logger.info(f'{homepage}')
 
+        print(price_change_percentage_24h)
+
         # read data from json file
         data=read_json_data("testdata/api_data/bitcoin.json")
         print(data)
         currency_data=data["bitcoin"]["currencies"]
         
-        assert response.status==data["bitcoin"]["expected_status_code"]
+        assert response.status==data["bitcoin"]["expected_status_code"],\
+              f"Expected status {data['bitcoin']['expected_status_code']}, " \
+              f"but received {response.status}"
         for currency in currency_data:
-            assert current_price[currency] is not None  and  current_price[currency]>0 and isinstance(current_price[currency], (int, float)), f'{current_price[currency]} is not present in current_price or price may be 0'
-            assert market_cap[currency] is not None  and  market_cap[currency]>0 and isinstance(market_cap[currency], (int, float)), f'{market_cap[currency]} is not present in market_cap or market_cap price  may be 0'
-            assert total_volume[currency] is not None  and  total_volume[currency]>0 and isinstance(total_volume[currency], (int, float)), f'{total_volume[currency]} is not present in total_volume or total_volume price  may be 0'
+            assert( currency in current_price 
+                   and current_price[currency] is not None  
+                    and  current_price[currency]>0
+                    and isinstance(current_price[currency], (int, float))), (
+                    f"Invalid current price for {currency}: "f"{current_price.get(currency)}")
+            
+            assert (currency in market_cap 
+                    and market_cap[currency] is not None  
+                    and  market_cap[currency]>0 
+                    and isinstance(market_cap[currency], (int, float))),(
+                    f"Invalid market cap for {currency}: "f"{market_cap.get(currency)}")
+            
+            assert( currency in total_volume
+                   and total_volume[currency] is not None  
+                   and  total_volume[currency]>0 
+                   and isinstance(total_volume[currency], (int, float))), (
+                   f'{total_volume[currency]} is not present in total_volume or total_volume price  may be 0')
 
-
-        assert homepage is not [] and homepage[0]!="", f'homepage: {homepage} is blank'
+        assert (price_change_percentage_24h is not None
+               and isinstance(price_change_percentage_24h, (int, float))
+               and -100 <= price_change_percentage_24h <= 100),\
+               f"Invalid 24h price change: {price_change_percentage_24h}"    
+            
+        assert (homepage and homepage[0].strip() != ""), (
+        f"Homepage is blank: {homepage}")
+        
         logger.info("end get bitcoin api test")
         
         
--- tests/ui/test_add_to_card.py
+++ tests/ui/test_add_to_card.py
@@ -18,26 +18,36 @@ class TestAddToCard:
         """
         Scenario 1 – Verify item can be added to Cart
         """
-        logger.info("cart verify test started")
+        logger.info("cart verification test started")
         home_page=HomePage(page)
         search_page=SearchPage(page)
-        
-        home_page.search_products(data["search_product"])
-        # click product will open new tab item_page returing new tab page
-        new_item_page=search_page.item_page()
 
-        logger.info(f" Current URL: {new_item_page.url}")
-        logger.info(f"Title:{ new_item_page.title()}")
-        new_home_page=HomePage(new_item_page)
-        item_page=ItemPage(new_item_page)
+        # Step 1: Search for the product.
+        home_page.search_products(data["search_product"])
+
+        # Step 2: Open the first product.
+        # This opens the product in a new tab/page
+        item_page_tab=search_page.item_page()
+
+        logger.info(f" Current URL: {item_page_tab.url}")
+        logger.info(f"Title:{ item_page_tab.title()}")
+
+        # Step3 : Create Page Objects using the new tab.
+        new_home_page=HomePage(item_page_tab)
+        item_page=ItemPage(item_page_tab)
+
+        # Step 4: Verify "Added to cart" confirmation is displayed.
         item_page.click_add_to_cart()
+
+        #step 5 Verify "Added to cart" confirmation is displayed.
         expect(item_page.added_to_cart_close).to_be_visible(timeout=10000)
-        
+
+       # Step 6: Close the "Added to cart" confirmation.
         item_page.click_added_to_cart_close()
-       
+       #Step 7: Verify cart count.
         expect(new_home_page.num_items()).to_have_text(str(data["expected_cart_count"]))
 
-        logger.info("cart verify test ended")
+        logger.info("Cart verification test completed successfully")
 
         
 
